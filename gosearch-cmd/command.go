b4d518022946b92f65b78cc7a07c542b0a4a2a55
Luke Brady <lbrad23105@gmail.com>
2018-10-03 00:17:15 -0400

    Can now index files from anywhere in the file system for searching.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- gosearch-cmd/command.go
+++ gosearch-cmd/command.go
@@ -4,6 +4,8 @@ import "encoding/gob"
 import "bytes"
 import "os"
 
+import "path/filepath"
+
 // GoSearchCMD is the command line object that is used to control the gosearch utility.
 type GoSearchCMD struct {
 	Index *InvertedIndex
@@ -33,14 +35,16 @@ func EncodeMap(tokenMap *InvertedIndex, encodedIndex chan []byte) {
 
 // IndexCMD is the function that indexes a file into memory that can be searched.
 func (cmd *GoSearchCMD) IndexCMD(path string) {
+	// Get the filename from the filepath.
+	fileName := filepath.Base(path)
 	// Now run the cmd.Index.IndexFile(path) function to index the file path supplied to the command.
-	cmd.Index.IndexFile(path)
+	cmd.Index.IndexFile(fileName)
 	// Make the channel to retrieve encoded data.
 	byteChan := make(chan []byte)
 	// Now serialize the new index to disk so that it can be used later to search.
 	go EncodeMap(cmd.Index, byteChan)
 	// Now write to disk.
-	writePath := "gosearch-cmd/tmp/" + path + ".gob"
+	writePath := "gosearch-cmd/tmp/" + fileName + ".gob"
 	indexFile, err := os.OpenFile(writePath, os.O_CREATE|os.O_RDWR, 0666)
 	if err != nil {
 		panic(err)
